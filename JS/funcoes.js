268d15a64a2b45ea22daa45513fc3a7b88b554ed
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-06-19 11:51:57 -0300

    atualizando.

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,6 +1,9 @@
 const voltar = document.getElementById('voltar');
+const voltar2 = document.getElementById('voltar2');
 const qrcode = document.getElementById('qrcode_chave')
+const qrcode2 = document.getElementById('qrcode_chave2')
 const pix = document.getElementById('pix')
+const pix2 = document.getElementById('pix2')
 
 
 pix.addEventListener('click', function(){
@@ -9,5 +12,13 @@ pix.addEventListener('click', function(){
 
 voltar.addEventListener('click', function(){
     qrcode.style.display='none'
+})
+
+pix2.addEventListener('click', function(){
+    qrcode2.style.display='block'
+})
+
+voltar2.addEventListener('click', function(){
+    qrcode2.style.display='none'
 })
 

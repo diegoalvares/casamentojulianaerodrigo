64b26adcf351a43e17ab86258c4ed6a5e6131c12
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-06-20 12:12:10 -0300

    atualizando.

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -2,14 +2,29 @@ const voltar = document.getElementById('voltar');
 const voltar2 = document.getElementById('voltar2');
 const voltar3 = document.getElementById('voltar3');
 const voltar4 = document.getElementById('voltar4');
+const voltar5 = document.getElementById('voltar5');
+const voltar6 = document.getElementById('voltar6');
+const voltar7 = document.getElementById('voltar7');
+const voltar8 = document.getElementById('voltar8');
+const voltar9 = document.getElementById('voltar9');
 const qrcode = document.getElementById('qrcode_chave')
 const qrcode2 = document.getElementById('qrcode_chave2')
 const qrcode3 = document.getElementById('qrcode_chave3')
 const qrcode4 = document.getElementById('qrcode_chave4')
+const qrcode5 = document.getElementById('qrcode_chave5')
+const qrcode6 = document.getElementById('qrcode_chave6')
+const qrcode7 = document.getElementById('qrcode_chave7')
+const qrcode8 = document.getElementById('qrcode_chave8')
+const qrcode9 = document.getElementById('qrcode_chave9')
 const pix = document.getElementById('pix')
 const pix2 = document.getElementById('pix2')
 const pix3 = document.getElementById('pix3')
 const pix4 = document.getElementById('pix4')
+const pix5 = document.getElementById('pix5')
+const pix6 = document.getElementById('pix6')
+const pix7 = document.getElementById('pix7')
+const pix8 = document.getElementById('pix8')
+const pix9 = document.getElementById('pix9')
 
 
 pix.addEventListener('click', function(){
@@ -42,4 +57,44 @@ pix4.addEventListener('click', function(){
 
 voltar4.addEventListener('click', function(){
     qrcode4.style.display='none'
+})
+
+pix5.addEventListener('click', function(){
+    qrcode5.style.display='block'
+})
+
+voltar5.addEventListener('click', function(){
+    qrcode5.style.display='none'
+})
+
+pix6.addEventListener('click', function(){
+    qrcode6.style.display='block'
+})
+
+voltar6.addEventListener('click', function(){
+    qrcode6.style.display='none'
+})
+
+pix7.addEventListener('click', function(){
+    qrcode7.style.display='block'
+})
+
+voltar7.addEventListener('click', function(){
+    qrcode7.style.display='none'
+})
+
+pix8.addEventListener('click', function(){
+    qrcode8.style.display='block'
+})
+
+voltar8.addEventListener('click', function(){
+    qrcode8.style.display='none'
+})
+
+pix9.addEventListener('click', function(){
+    qrcode9.style.display='block'
+})
+
+voltar9.addEventListener('click', function(){
+    qrcode9.style.display='none'
 })
